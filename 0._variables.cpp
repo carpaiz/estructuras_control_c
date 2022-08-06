e0ac7c520b907f9bf504985537fc61a84f14942f
carpaiz <32883274+carpaiz@users.noreply.github.com>
2022-08-06 09:03:35 -0600

    anotaciones y comentarios

--- 0._variables.cpp
+++ 0._variables.cpp
@@ -2,21 +2,33 @@
 
 using namespace std;
 main(){
+	//C++ es case-sensitive:   distinguen mayúsculas de minúsculas
+	//C++ es un lenguaje compilado y fuertemente tipado
+	// Una Variable Es un espacio reservado en memoria que se utiliza para almacenar un valor que corresponde a un tipo de dato.
+	// Estilo de nombres para variables: snake case ejemplo String nombre_completo;
+	// restricciones nombre de variable: no puede iniciar por número, no puede contener caracteres especiales, no espacios en blanco
+	// las variables deben de tener nombres lógicos
 	
+	
+
 //Variables Numericas
 //Numero Enteros
-/*Variable: Espacio reservado en memoria*/
-short sh=2; //short 2Bytes (-32768 a 32767)
-int in =4; //4 Bytes -2147483648 a +2147483647
+
+short sh=32767; //short 2Bytes (-32768 a 32767)
+int in =2147483647; //4 Bytes -2147483648 a +2147483647
+long long lg = 52147483647; // 8 bytes -(2^63) to (2^63)-1
 unsigned int in2=5;
 signed int in3=-5;
-long lg = 4.25;
+unsigned  long long int entero_largo = 18446744073709551615; //  8 bytes 0 to 18,446,744,073,709,551,615
+
 cout<<"------ Enteros ------"<<endl;
 	cout<<"short: "<<sh<<endl;
 	cout<<"int: "<<in<<endl;
+	cout<<"long: "<<lg<<endl;
 	cout<<"int Positivo: "<<in2<<endl;
 	cout<<"int Negativo: "<<in3<<endl;
-	cout<<"long: "<<lg<<endl;
+	cout<<"unsigned  long long int: "<<entero_largo<<endl;
+	
 // Numero reales de punto flotante
 float flo = 100.253;// 4 bytes
 double dob=200.253;// 8 Bytes
