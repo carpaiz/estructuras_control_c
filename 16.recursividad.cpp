5ad257740fda03a6f3c09431f5bb7b0effeca534
carpaiz <32883274+carpaiz@users.noreply.github.com>
2023-09-09 10:00:25 -0600

    comite funciones recursivas

--- 16.recursividad.cpp
+++ 16.recursividad.cpp
@@ -3,20 +3,36 @@ using namespace std;
 int temporizador(int n);
 int factorial(int n);
 main(){
-//El factorial se simboliza como n!, se lee como "n factorial", y la definición es:
+//El factorial se simboliza como n!, se lee como "n factorial", y la definiciÃ³n es:
 //n! = n * (n-1) * (n-2) * ... * 1
+
 int num =0;
 cout<<"Ingrese Numero:";
 cin>>num;
+
+cout<<" ---------- Funciones recursivas ---------"<<endl;
 cout<<temporizador(num)<<endl;
 cout<<"------------"<<endl;
 cout<<"El factorial es: "<<factorial(num)<<endl;
+
+cout<<" ---------- Con iteraciones---------"<<endl;
+cout<<"----------- Temporizador : "<<endl;
+int acumulador=1;
+for (int i = num; i>=0;i--){
+		cout<<i<<endl;
+	if (i>0){
+		acumulador *=i;
+	}
+	
+	
+}
+cout<<"----------- Factorial: "<<endl;
+cout<<acumulador<<endl;
 system("PAUSE");
 }
 	
 int temporizador(int n){
-	
-	if (n>0){
+	if (n > 0){
 		cout<<n<<endl;
 		n = temporizador(n-1);
 	}else {
@@ -26,7 +42,7 @@ int temporizador(int n){
 	
 }
 /*Hay algunas limitaciones:
-No es posible calcular el factorial de números negativos, no está definido.
+No es posible calcular el factorial de nÃºmeros negativos, no estÃ¡ definido.
 El factorial de cero es 1.
 */
 int factorial(int n) {
